05a5271ce4461a1c73378ba69ed80145f05a3399
Alexey <melentev@biocad.ru>
2023-12-12 01:05:52 +0300

    added locales

---
+++ src/app/locales/initLocales.ts
@@ -0,0 +1,13 @@
+import intl from "react-intl-universal";
+
+const locales = {
+  "ru-RU": require("./ru-RU.json"),
+  "en-US": require("./en-US.json"),
+};
+
+export const initLocales = (name): void => {
+  intl.init({
+    currentLocale: name,
+    locales,
+  });
+};
